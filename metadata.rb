f16810e3d0997c45c3b8da2040b075e7864bb7bb
pauloricardomg <pauloricardomg@gmail.com>
2014-12-10 16:16:03 -0200

    changed home variable to user_home (due to conflict in some other cookbook)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "enis@speeddate.com"
 license          "Apache 2.0"
 description      "Installs and configures s3cmd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.4"
+version          "0.0.5"
 
 recipe "s3cmd", "Installs and configures s3cmd"
 
