cd29f77c0c887772e1fdad86af06cfd6a0c2b086
Flávio Roberto Santos <flavio.barata@gmail.com>
2014-12-29 16:00:32 -0200

    Bumping

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "enis@speeddate.com"
 license          "Apache 2.0"
 description      "Installs and configures s3cmd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.8"
+version          "0.0.9"
 
 recipe "s3cmd", "Installs and configures s3cmd"
 
